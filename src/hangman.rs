1e6d15202faf0747f1dabe1eb79d2999d4301498
Eduardo P <edtvports@gmail.com>
2022-09-01 16:10:45 -0300

    Cleaned code
    Improvements to legibility and moved code to function.

--- src/hangman.rs
+++ src/hangman.rs
@@ -1,26 +1,22 @@
 use std::{io, i8};
 use rand::{self, thread_rng, Rng};
 
-pub fn play() -> io::Result<()> {
-    let allowed_chars = vec!["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",];
+const RED: &str = "\x1b[31m";
+const GREEN: &str = "\x1b[32m";
+const YELLOW: &str = "\x1b[33m";
+const END: &str = "\x1b[0m";
 
-    let words = vec!["macarrao", "salsicha", "batata", "guaravita", "sal", "antofagasta", "peba"];
+pub fn play() -> io::Result<()> {
+    let allowed_chars = vec!["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+
+    let words = vec!["PASTA", "SAUSAGE", "POTATO", "SODA", "SODIUM", "ANTOFAGASTA", "BRAZIL", "FUNNY", "COOL"];
     let selector = thread_rng().gen_range(0..words.len());
     let word = String::from(words[selector]);
 
     let mut lives: i8 = 5;
 
     let mut inp_chars: Vec<char> = Vec::new();
-    let mut characters: Vec<char> = Vec::new();
-
-    for letter in word.chars() {
-        if characters.contains(&letter) {
-            continue;
-        }
-        else {
-            characters.push(letter);
-        }
-    }
+    let characters: Vec<char> = word_characters(&word);
 
     println!(r"
      _
@@ -30,17 +26,21 @@ pub fn play() -> io::Result<()> {
     | | | | (_| | | | | (_| | | | | | | (_| | | | |
     |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                         __/ |
-                       |___/  ");
+                       |___/  
+
+         Type in letters to guess the word...");
+         
     loop {
+
+        println!("\n{YELLOW}{}{END}", print_word(&word, &inp_chars));
+        println!("{YELLOW}Lives remaing: {}{END}", lives);
+
         let mut input = String::new();
-
-        println!("\n\x1b[33m{}\x1b[0m", print_word(&word, &inp_chars));
-        println!("\x1b[33mLives remaing: {}\x1b[0m", lives);
-
         io::stdin().read_line(&mut input)?;
-        let input = input.trim().to_lowercase(); 
-        let input = input.as_str();
+        let input = input.to_uppercase(); 
+        let input = input.trim();
 
+        println!("{}", input);
         if allowed_chars.contains(&input) {
             let input = input.trim().chars();
 
@@ -59,11 +59,11 @@ pub fn play() -> io::Result<()> {
             }
 
             if word == print_word(&word, &inp_chars) {
-                println!("\n\x1b[32mYou Won!\nThe word was {}!\x1b[0m", word);
+                println!("\n{GREEN}You Won!\nThe word was {}!\n{END}", word);
                 break;
             }
             if lives == 0 {
-                println!("\n\x1b[31mYou Lost!\nThe word was '{}'!\x1b[0m", word);
+                println!("\n{RED}You Lost!\nThe word was '{}'!\n{END}", word);
                 break;
             }
         }
@@ -72,6 +72,19 @@ pub fn play() -> io::Result<()> {
         }
     }
     Ok(())
+}
+
+fn word_characters(word: &String) -> Vec<char> {
+    let mut characters = vec![];
+    for letter in word.chars() {
+        if characters.contains(&letter) {
+            continue;
+        }
+        else {
+            characters.push(letter);
+        }
+    }
+    return characters;
 }
 
 fn print_word (all: &String, allowed: &Vec<char>) -> String{
